2c60ae8470c9e1557111a5c53778d2e1b1e745fc
Silme94 <104931422+Silme94@users.noreply.github.com>
2023-10-29 22:30:11 +0100

    Add files via upload

---
+++ Server/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+
+class Server
+{
+    public static List<Socket> clients = new List<Socket>();
+    public static void Main(string[] args)
+    {
+        using (Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+        {
+            server.Bind(new IPEndPoint(IPAddress.Parse(args[0]), int.Parse(args[1])));
+            server.Listen(10);
+
+            Console.WriteLine($"Server Chat is listening at {args[0]}:{args[1]}");
+
+            while (true)
+            {
+                Socket client = server.Accept();
+                Console.WriteLine(client.RemoteEndPoint + " has joined the chat.");
+                clients.Add(client);
+
+                Thread handle = new Thread(() => { HandleClient(client); });
+                handle.Start();
+            }
+        }
+    }
+
+    public static void HandleClient(Socket client)
+    {
+        try
+        {
+            while (true)
+            {
+                byte[] buffer = new byte[2048];
+                int bytesRead = client.Receive(buffer);
+                if (bytesRead == 0)
+                {
+                    Console.WriteLine(client.RemoteEndPoint + " has left the chat.");
+                    clients.Remove(client);
+                    client.Close(); 
+                    break;
+                }
+
+                string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine(client.RemoteEndPoint + " > " + message);
+
+                foreach (Socket cl in clients)
+                {
+                    if (cl != client)
+                    {
+                        cl.Send(Encoding.ASCII.GetBytes(client.RemoteEndPoint + " > " + message));
+                    }
+                }
+            }
+        }
+        catch
+        {
+            Console.WriteLine(client.RemoteEndPoint + " has left the chat.");
+            clients.Remove(client);
+            client.Close();
+        }
+    }
+
+}
---
+++ TerminalChat/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Server
+{
+    public static async Task Main(string[] args)
+    {
+        Console.Write("Enter IP server => ");
+        string ip = Console.ReadLine();
+
+        Console.Write("Enter PORT server => ");
+        int port = int.Parse(Console.ReadLine());
+
+        using (Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+        {
+            Console.WriteLine("Connecting...");
+            client.Connect(new IPEndPoint(IPAddress.Parse(ip), port));
+            Console.WriteLine("Connected!");
+
+            _ = Task.Run(() =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        if (client.Connected)
+                        {
+                            byte[] buffer = new byte[1024];
+                            int bytesRead = client.Receive(buffer);
+
+                            if (bytesRead > 0)
+                            {
+                                string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                                Console.WriteLine(message);
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Disconnected : " + e.Message);
+                }
+            });
+
+            while (true)
+            {
+                Console.Write(">> ");
+                string message = Console.ReadLine();
+                client.Send(Encoding.ASCII.GetBytes(message));
+            }
+        }
+    }
+}
